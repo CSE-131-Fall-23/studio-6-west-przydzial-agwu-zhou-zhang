a6df9b670591e6d355679680f52e8ee386c7b7c2
livprzydzial <livprzydzial@localhost>
2023-10-25 14:23:15 -0500

    still working on last part

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -1,5 +1,7 @@
 package studio6;
 
+import java.lang.reflect.Array;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class RecursiveMethods {
@@ -14,7 +16,13 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			// base case
+		if(n==0) {
+			return 0;}
+		else {
+			return geometricSum(n-1)+Math.pow(0.5, n);
+			
+		}
 		
 	}
 
@@ -29,11 +37,14 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+			if(q==0) {
+				return p;
+			}
+			else {
+				return(gcd(q,p%q));
 		
 	}
-
-	
+	}
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
@@ -41,13 +52,31 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
-	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+	public static int[] toReversed(int[] array) {	
+		int [] reversed = new int [array.length];
+		else { // for test string: 1234, length = 4, 4321
+			int index = array.length-1; // last index of array which will be our reversed[0]
+			recursion(array, index);
+			
+			reversed[array.length-index] = ;
+			index--;
+			// pull values out + put into new array one by one
+		}
+		return reversed;
 	}
-
+	
+	private static int recursion(int [] array, int index) { // helper method is NOT recursive
+		int [] reversed = new int [array.length];
+		if(index == 0){
+			return array[0];
+		}
+		else {
+			reversed[array.length-1-index] = array[index];
+			return recursion(array,index-1);
+		}
+	}
+				
+	
 	/**
 	 * @param xCenter                       x-coordinate of the center of the circle
 	 *                                      at the current depth
